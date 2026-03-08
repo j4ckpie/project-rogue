92e460f49a2c4495f85d7a729e58e21daab6cff2
Jakub Pietrala <j4ckpie@users.noreply.github.com>
2026-03-08 21:11:14 +0100

    Added sprinting and turning

--- godot/src/PlayerScript.cs
+++ godot/src/PlayerScript.cs
@@ -4,8 +4,18 @@ using System;
 [GlobalClass]
 public partial class PlayerScript : CharacterBody2D
 {
-	float health = 100.0f;
-	float movementSpeed = 100.0f;
+	public enum MovementMode { IDLE, WALK, SPRINT, ATTACK, SHOOT }
+
+	[Export]
+	private Sprite2D _playerSprite;
+	[Export]
+	private AnimationPlayer _animationPlayer;
+	private float _health = 100.0f;
+	private float _movementSpeed = 100.0f;
+	private Vector2 _direction = Vector2.Zero;
+	private bool _isSprinting = false;
+	private MovementMode _movementMode = MovementMode.IDLE;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -14,15 +24,46 @@ public partial class PlayerScript : CharacterBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Vector2 direction = Vector2.Zero;
-		direction.X = Input.GetActionStrength("right") - Input.GetActionStrength("left");
-		direction.Y = Input.GetActionStrength("down") - Input.GetActionStrength("up");
-		Velocity = direction * movementSpeed;
 	}
 
     public override void _PhysicsProcess(double delta)
     {
+		_direction.X = Input.GetActionStrength("right") - Input.GetActionStrength("left");
+		_direction.Y = Input.GetActionStrength("down") - Input.GetActionStrength("up");
+		_isSprinting = Input.GetActionStrength("sprint") > 0 ? true : false;
+		if(_isSprinting) Velocity = _direction * _movementSpeed * 1.75f;
+		else Velocity = _direction * _movementSpeed;
         MoveAndSlide();
+		UpdatePlayerAnimation();
     }
+
+	private void UpdatePlayerAnimation()
+	{
+		UpdateSpriteDirection();
+		if(Velocity.Length() ==  0)
+		{
+			_movementMode = MovementMode.IDLE;
+			_animationPlayer.Play("idle");
+		}
+		else if(Velocity.Length() > 0)
+		{
+			_movementMode = MovementMode.WALK;
+			_animationPlayer.Play("walk");
+			if(_isSprinting) _animationPlayer.SpeedScale = 1.075f;
+			else _animationPlayer.SpeedScale = 1.0f;
+		}
+	}
+
+	private void UpdateSpriteDirection()
+	{
+		if(GlobalPosition.X - GetGlobalMousePosition().X > 0)
+		{
+			_playerSprite.FlipH = true;
+		}
+		else if(GlobalPosition.X - GetGlobalMousePosition().X < 0)
+		{
+			_playerSprite.FlipH = false;
+		}
+	}
 
 }
