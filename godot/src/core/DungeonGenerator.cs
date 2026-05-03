fa80fb4cb12a99e62c499c72fe83dc9a3f9fbdfb
Jakub Pietrala <j4ckpie@users.noreply.github.com>
2026-05-03 22:25:00 +0200

    Completed dungeon generation

--- godot/src/core/DungeonGenerator.cs
+++ godot/src/core/DungeonGenerator.cs
@@ -2,8 +2,9 @@ using Godot;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
-public partial class DungeonGenerator : Node
+public partial class DungeonGenerator : Node2D
 {
     [ExportGroup("Nodes")]
     [Export]
@@ -17,27 +18,27 @@ public partial class DungeonGenerator : Node
     private Queue<Marker2D> _exitsToProcess = new Queue<Marker2D>();
 
     public override void _Ready()
-	{
-		Generate();
-	}
+    {
+        GenerateAsync();
+    }
 
-    private void Generate()
+    private async void GenerateAsync()
     {
         Node2D startRoom = StartRooms.PickRandom().Instantiate<Node2D>();
         AddChild(startRoom);
         startRoom.GlobalPosition = Vector2.Zero;
+        await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
 
         AddExitsToQueue(startRoom);
 
         while(_exitsToProcess.Count > 0)
         {
             Marker2D currentExit = _exitsToProcess.Dequeue();
-            PackedScene pickedRoom = _currentRoomCount < 2
+            PackedScene pickedRoom = _currentRoomCount < 30
                 ? PickCompatibleRoom(MidRooms, currentExit)
                 : PickCompatibleRoom(EndRooms, currentExit);
 
-            //if(pickedRoom == null && _currentRoomCount < 2) pickedRoom = PickCompatibleRoom(EndRooms, currentExit);
-            if(pickedRoom != null) SpawnRoom(pickedRoom, currentExit);
+            if(pickedRoom != null) await SpawnRoomAsync(pickedRoom, currentExit);
         }
     }
 
@@ -49,7 +50,7 @@ public partial class DungeonGenerator : Node
             if(CheckRoomCompatibility(room, exit)) return room;
         }
         return null;
-}
+    }
 
     private bool CheckRoomCompatibility(PackedScene roomPrefab, Marker2D exit)
     {
@@ -63,13 +64,28 @@ public partial class DungeonGenerator : Node
         return isCompatible;
     }
 
-    private void SpawnRoom(PackedScene roomPrefab, Marker2D exit)
+    private async Task SpawnRoomAsync(PackedScene roomPrefab, Marker2D exit)
     {
         Room newRoom = roomPrefab.Instantiate<Room>();
-        AddChild(newRoom);
-
         Marker2D entrance = newRoom.GetNode<Marker2D>("Entrance");
-        newRoom.GlobalPosition = exit.GlobalPosition - entrance.Position;
+        Vector2 newPosition = exit.GlobalPosition - entrance.Position;
+
+        AddChild(newRoom);
+        newRoom.GlobalPosition = newPosition;
+        
+        await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
+
+        Area2D area = newRoom.GetNode<Area2D>("Area2D");
+        var overlapping = area.GetOverlappingAreas();
+        int overlapCount = overlapping.Count(a => a != area);
+
+        if(overlapCount > 0)
+        {
+            newRoom.QueueFree();
+            PackedScene endRoom = PickCompatibleRoom(EndRooms, exit);
+            if(endRoom != null) await SpawnRoomAsync(endRoom, exit);
+            return;
+        }
 
         _currentRoomCount++;
         AddExitsToQueue(newRoom);
